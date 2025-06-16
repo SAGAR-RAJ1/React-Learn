4ffc4620d7c6b965b8ec0e8c200e9b75468985fd
SAGAR-RAJ1 <157152445+SAGAR-RAJ1@users.noreply.github.com>
2025-06-16 21:27:18 +0530

    working on event

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 
 import './App.css'
+import Button from './Button'
 
 function App() {
 
   return (
     <>
   
+<div>
+<h1>Button will be here</h1>
 
-   
+</div>
+   <Button/>
      
     </>
     
---
+++ src/Button.jsx
@@ -0,0 +1,11 @@
+function PRint(){
+console.log("wow i am working")
+}
+
+function Button(){
+  return ( 
+  <button onClick={PRint}>Click to Toggle Theme</button>
+  
+)
+}
+export default Button;
