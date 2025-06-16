d33d6f154df59dfd54ef57b426264ac5f85febe2
SAGAR-RAJ1 <157152445+SAGAR-RAJ1@users.noreply.github.com>
2025-06-16 21:33:30 +0530

    corrected github

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 
 import './App.css'
-<<<<<<< HEAD
 import Button from './Button'
-=======
-// import HY from "./Title" Method 2
-import {HY} from "./Title"
->>>>>>> 36cf387ad573fe3d651f1937cc05bf510978a1b9
 
 function App() {
 let options =["Sagar","Aditya","Ayush"];
@@ -15,17 +10,9 @@ let object={
 };
   return (
     <>
-<<<<<<< HEAD
   
 <div>
 <h1>Button will be here</h1>
-=======
-    <HY name="Saarya" names={options} object={object} />
-  <h1 >HEllo</h1>
-  // If you not pass the names react eill handle it but accessing object
-  // like obj.a will hrow error
-  <HY name="Indrajeet" object={object}></HY>
->>>>>>> 36cf387ad573fe3d651f1937cc05bf510978a1b9
 
 </div>
    <Button/>
