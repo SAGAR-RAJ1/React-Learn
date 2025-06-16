fac075614f9baa05d1602213a1ece956620b73cb
SAGAR-RAJ1 <157152445+SAGAR-RAJ1@users.noreply.github.com>
2025-06-16 21:26:42 +0530

    Event Handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
 
 import './App.css'
 
-function HY(){
-
- return   <h1>HEllo brother</h1>
- 
-  
-}
-
 function App() {
 
   return (
     <>
-    <HY/>
-  <h1>HEllo</h1>
-  <HY></HY>
+  
 
    
      
