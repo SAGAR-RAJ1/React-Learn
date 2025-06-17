b906e64719b7da15c01555b9b95a7b83fd9f293a
SAGAR-RAJ1 <157152445+SAGAR-RAJ1@users.noreply.github.com>
2025-06-17 09:29:10 +0530

    Event object

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,15 @@ import './App.css'
 import Button from './Button'
 
 function App() {
-let options =["Sagar","Aditya","Ayush"];
-let object={
-  s:"Sagar",
-  a:"Adi"
-};
+
   return (
     <>
   
 <div>
 <h1>Button will be here</h1>
-
+<Button/>
 </div>
-   <Button/>
+   
      
     </>
     
--- src/Button.jsx
+++ src/Button.jsx
@@ -1,5 +1,7 @@
-function PRint(){
+function PRint(object){
+    //Every event has an object 
 console.log("wow i am working")
+console.log(object)
 }
 
 function Button(){
