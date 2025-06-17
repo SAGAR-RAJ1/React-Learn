42495298095415d88e89e2cb62b613aa1513fc5d
SAGAR-RAJ1 <157152445+SAGAR-RAJ1@users.noreply.github.com>
2025-06-17 09:45:36 +0530

    UseState with eg to inc count

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,12 +1,16 @@
-function PRint(object){
-    //Every event has an object 
-console.log("wow i am working")
-console.log(object)
-}
+import { useState } from "react";
+
 
 function Button(){
+    const [count, setcount] = useState(0);
+function incCount() {
+ setcount(count+1)
+}
   return ( 
-  <button onClick={PRint}>Click to Toggle Theme</button>
+    <div>
+   <button onClick={incCount}>Click to increase count</button>
+       <h3>Count : {count}</h3>
+    </div>
   
 )
 }
